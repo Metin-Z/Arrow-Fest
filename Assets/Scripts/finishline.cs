69202d9825a850452e803daec01659772dad268f
Metin-Z <102662790+Metin-Z@users.noreply.github.com>
2022-08-11 22:32:45 +0300

    confetti

--- Assets/Scripts/finishline.cs
+++ Assets/Scripts/finishline.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class finishline : MonoBehaviour
 {
     LevelManager _levelManager;
+    public List<GameObject> Confettis;
+    public Transform ConfPos;
     void Awake()
     {
         _levelManager = FindObjectOfType<LevelManager>();
@@ -19,6 +22,7 @@ public class finishline : MonoBehaviour
             LevelManager.EndActive = true;
             _levelManager.nextLevelUI.SetActive(LevelManager.EndActive);
             //gameObject.SetActive(false);
+            Instantiate(Confettis[Random.Range(0,7)],ConfPos);
         }
     }
 }
