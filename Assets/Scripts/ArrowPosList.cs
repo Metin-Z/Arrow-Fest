3c8ab47edc2cb72618ecdd5a17cb63ce9d1a5d8e
Metin-Z <102662790+Metin-Z@users.noreply.github.com>
2022-08-16 13:15:29 +0300

    pos random

--- Assets/Scripts/ArrowPosList.cs
+++ Assets/Scripts/ArrowPosList.cs
@@ -11,23 +11,27 @@ public class ArrowPosList : MonoBehaviour
     public void Start()
     {
         float radius = 0.075f;
-        float randomMin = -0.04f;
-        float randomMax = 0.04f;
+        float randomMinY = 0f;
+        float randomMaxY = 0.5f;
+        float randomMinX = 0f;
+        float randomMaxX = 0f;
         for (int i = 0; i < ArrowSpawnList.Length; i++)
         {
             Debug.Log("Pos List For");
             if (i % 10 == 0)
             {
                 radius += 0.15f;
-                randomMin -= 0.07f;
-                randomMax -= 0.07f;
+                randomMinY -= 0.07f;
+                randomMaxY -= 0.07f;
+                randomMinX -= 0.03f;
+                randomMaxX += 0.01f;
                 Debug.Log("10 a böldü");
             }
 
 
-            float angle = ((i * Mathf.PI * 2 / 10) + Random.Range(0.5F, 2.5F)) % 360;
-            float y = (Mathf.Sin(angle) * radius) + Random.Range(randomMin, randomMax);
-            float x = (Mathf.Cos(angle) * radius) + Random.Range(randomMin, randomMax);
+            float angle = ((i * Mathf.PI * 2 / 10) + Random.Range(0, 2)) % 360;
+            float y = (Mathf.Sin(angle) * radius) + Random.Range(randomMinY,randomMaxY);
+            float x = (Mathf.Cos(angle) * radius) + Random.Range(randomMinX, randomMaxX);
 
             Vector3 pos = gameObject.GetComponent<ArrowPosList>().ArrowSpawnList[i].transform.position = new Vector3(x, y + 2.5f, 0);
             float angleDegrees = -angle * Mathf.Rad2Deg;
